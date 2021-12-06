3bf92f54bb2d07238fbdc845f4d30bbdebb1c486
AjayReddyK <ajay45177@gmail.com>
2021-12-06 16:32:39 +0530

    first commit

---
+++ DataSizeRange.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+
+class DataSizeRange{
+    public static void main(String args[]){
+        System.out.println("Int minimum:"+Integer.MIN_VALUE);
+        System.out.println("Int maximum:"+Integer.MAX_VALUE);
+        System.out.println("Int Bytes:"+Integer.BYTES);
+    }
+}
